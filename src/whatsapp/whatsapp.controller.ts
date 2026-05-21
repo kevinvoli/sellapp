c6ad4baa7d42e114c5347703692031c315546695
kevin voli <gbamblekevin@gmail.com>
2026-05-21 14:04:36 +0000

    szertyj

--- src/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { SendMessageDto } from './dto/send-message.dto';
 import { WhatsappService } from './whatsapp.service';
 
+
 @Controller('whatsapp')
 export class WhatsappController {
   constructor(private readonly whatsappService: WhatsappService) {}
